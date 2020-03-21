fdf0451e2a50625531324e7cf86a005933eb8bdc
Rohith GB <rohit.gb@preceptlabs.com>
2020-03-21 14:36:32 +0530

    async/await express middleware and bootcamp name slugifying

---
+++ middleware/error.js
@@ -0,0 +1,36 @@
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  let error = {
+    ...err
+  };
+
+  error.message = err.message;
+
+  console.log(err);
+
+  // if id not found
+  if (err.name === "CastError") {
+    error = new ErrorResponse(`Resource not found with id ${err.value}`, 404);
+  }
+
+  // duplicate field error
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  //mongoose field validation error [required]
+  if (err.name === "ValidatorError" || err.errors) {
+    const message = Object.values(err.errors).map(val => val.message);
+    console.log("error name", err.name);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server Error"
+  });
+};
+
+module.exports = errorHandler;
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,3 +1,4 @@
+const slugify = require("slugify");
 const mongoose = require("mongoose");
 
 const BootcampSchema = new mongoose.Schema({
@@ -112,6 +113,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
