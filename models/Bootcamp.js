66338dabe94efc14edac9a6d8591d9eb257e37ca
Rohith GB <rohit.gb@preceptlabs.com>
2020-03-21 15:11:40 +0530

    implemented node-geocoder, from address generated location object

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 const mongoose = require("mongoose");
 
 const BootcampSchema = new mongoose.Schema({
@@ -118,6 +119,26 @@ const BootcampSchema = new mongoose.Schema({
 // Create bootcamp slug from the name
 BootcampSchema.pre("save", function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Jusing address to generate location object
+BootcampSchema.pre("save", async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  console.log("location", loc);
+
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+  // do not save address in db
+  this.address = undefined;
   next();
 });
 
